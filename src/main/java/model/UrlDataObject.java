384b6e6090446e5e70b96cfb99e178a807e07abb
Jake Yeo <Jake Yeo@DESKTOP-P6JNU2B>
2022-02-15 12:04:47 -0700

    Updated method to get youtube playlist urls.

--- src/main/java/model/UrlDataObject.java
+++ src/main/java/model/UrlDataObject.java
@@ -11,17 +11,27 @@ package model;
 public class UrlDataObject {
 
     private String videoTitle;
+    private String videoUrl;
     private String videoDuration;
     private String channelName;
     private String thumbnailUrl;
     private String errorMessage;
     private boolean didErrorOccur;
 
+    public UrlDataObject(String videoTitle, String videoDuration, String channelName, String thumbnailUrl, String videoUrl) {
+        this.videoTitle = videoTitle;
+        this.videoDuration = videoDuration;
+        this.channelName = channelName;
+        this.thumbnailUrl = thumbnailUrl;
+        this.videoUrl = videoUrl;
+    }
+
     public UrlDataObject(String videoTitle, String videoDuration, String channelName, String thumbnailUrl) {
         this.videoTitle = videoTitle;
         this.videoDuration = videoDuration;
         this.channelName = channelName;
         this.thumbnailUrl = thumbnailUrl;
+        this.videoUrl = "";
     }
 
     public String getTitle() {
@@ -38,5 +48,9 @@ public class UrlDataObject {
 
     public String getThumbnailUrl() {
         return this.thumbnailUrl;
+    }
+
+    public String getVideoUrl() {
+        return this.videoUrl;
     }
 }
