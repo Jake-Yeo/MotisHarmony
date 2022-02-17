14c62d085dd1a230fc771ff37bd1a246b75bcef9
Jake-Yeo <jakeyeozh@gmail.com>
2022-02-17 01:08:23 -0700

    Added EncryptionDecryption class

--- src/main/java/model/EncryptionDecryption.java
+++ src/main/java/model/EncryptionDecryption.java
@@ -5,13 +5,65 @@
  */
 package model;
 
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 /**
  *
  * @author Jake Yeo
  */
 public class EncryptionDecryption {
 
-    public void init() {
+    private SecretKey key;
+    private int KEY_SIZE = 128;
+    private Cipher encryptionCipher;
+    private int T_LEN = 128;
 
+    public void init() throws Exception {
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        generator.init(KEY_SIZE);
+        key = generator.generateKey();
+    }
+
+    public String encrypt(String password) throws Exception {
+        byte[] passwordInBytes = password.getBytes();
+        encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        encryptionCipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedBytes = encryptionCipher.doFinal(passwordInBytes);
+        return encode(encryptedBytes);
+    }
+
+    public String decrypt(String encryptedPassword) throws Exception {
+        byte[] passwordInBytes = decode(encryptedPassword);
+        Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec spce = new GCMParameterSpec(T_LEN, encryptionCipher.getIV());
+        decryptionCipher.init(Cipher.DECRYPT_MODE, key, spce);
+        byte[] decryptedBytes = decryptionCipher.doFinal(passwordInBytes);
+        return new String(decryptedBytes);
+    }
+
+    private String encode(byte[] data) {
+        return Base64.getEncoder().encodeToString(data);
+    }
+
+    private byte[] decode(String data) {
+        return Base64.getDecoder().decode(data);
+    }
+
+    public String getSecretKey() {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    public void setSecretKey(SecretKey key) {
+        this.key = key;
+    }
+
+    public SecretKey stringToSecretKey(String key) {
+        byte[] decodedKey = Base64.getDecoder().decode(key);
+        return new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
     }
 }
